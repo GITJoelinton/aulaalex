079fd8ea367fb58fce94184aa0056e9b229c938c
Joelinton Siqueira <joelintoSiqueira@gmail.com>
2025-10-23 20:06:27 -0300

    meu

---
+++ back/cadastrar.php
@@ -0,0 +1,15 @@
+<?php
+$conn = new mysqli('sql213.byetcluster.com', 'if0_40124930', '1540867234', 'if0_40124930_logins');
+if ($conn->connect_error) {
+    die("erro de conexão: " . $conn->connect_error);
+}
+$usuario = $_POST['username'];
+$senha = $_POST['password'];
+$sql = "INSERT INTO users (username, senha) VALUES ('$usuario','$senha')";
+if ($conn->query($sql) === TRUE) {
+    echo "Cadastro realizado com sucesso! <a href='../front/login.html'> Fazer login</a>";
+} else {
+    echo "Erro ao cadastrar: " . $conn->error;
+}
+$conn->close();
+?>

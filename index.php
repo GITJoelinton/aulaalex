063e13020cd11659a249332cb424b37d76a8c214
Joelinton Siqueira <joelintoSiqueira@gmail.com>
2025-10-24 23:39:20 -0300

    meu

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
- $host = 'sql100.byetcluster.com';
-	 $dbname = 'icei_40249526_trabalho';
-	$user = 'icei_40249526';
-	 $pass = 'joel1240';
-$conn = new mysqli($host, $user, $pass, $db);
+ $host='sql100.byetcluster.com';
+	 $dbname='icei_40249526_trabalho';
+	$user='icei_40249526';
+	 $pass='joel1240';
+$conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
     die("erro de conexão: " . $conn->connect_error);
 }
